cd505a49bd3e1f90c0027a428f00f99743eb4564
0x1 <kevin.francart3@gmail.com>
2026-07-06 18:58:56 +0200

    feat: migrate FFI to exports array format

--- src/Effect.php
+++ src/Effect.php
@@ -1,13 +1,17 @@
 <?php
 
-$Effect_pureE = function($x) { return function() use(&$x) { return $x; }; };
-$Effect_bindE = function($a, $f = null) {
+$pureE = function($x) use (&$pureE) { return function() use(&$x) { return $x; }; };
+$bindE = function($a, $f = null) use (&$bindE) {
     if (func_num_args() < 2) {
         $__args = func_get_args();
-        return function(...$more) use ($__args) {
-            global $Effect_bindE;
-            return $Effect_bindE(...array_merge($__args, $more));
+        return function(...$more) use ($__args, &$pureE) {
+
+            return $bindE(...array_merge($__args, $more));
         };
     }
     return function() use(&$a, &$f) { return $f($a())(); };
 };
+
+$exports['pureE'] = $pureE;
+$exports['bindE'] = $bindE;
+return $exports;
